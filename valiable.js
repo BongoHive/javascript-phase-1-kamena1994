7b4fb9ad07fec0e9ca6608306e951220cb3201b3
kamena1994 <37587336+kamena1994@users.noreply.github.com>
2019-06-10 13:47:00 +0300

    variables
    added javascript

---
+++ valiable.js
@@ -0,0 +1,11 @@
+// // QUESTION: 1
+var birth_year = 25;
+var future_year = 27;
+var possible_ages = birth_year + future_year;
+
+//// QUESTION: 2
+var current_age = 25;
+var maximum_age = 70;
+var estimated_amount_per_day = 250;
+var total_rest_life = maximum_age * estimated_amount_per_day;
+console.log("You will need" + total_rest_life +  "to last you until the ripe old age of " + maximum_age)
